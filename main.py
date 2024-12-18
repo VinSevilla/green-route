736c05fe7c6bfeb56c4c2a0142a5bf3aa31cf029
“VinSevilla” <vls.sevilla@gmail.com>
2024-12-18 01:26:26 -0800

    Create list containing address of each checkpoint

--- main.py
+++ main.py
@@ -29,38 +29,42 @@ usps_html = BeautifulSoup(html, 'html.parser')
 
 shipping_address = ""
 destination = ""
+address = " "
 checkpoints = []
 
-# Because the webpage is dynamic, check shipping status of current step html element 
-current_step = usps_html.find_all(class_="tb-step current-step")[0]
-# get the shipping status from the html current step
-shipping_status = current_step.find_all(class_="tb-status-detail")
-# parcel the shipping status into a single string
-status = ""
-for words in shipping_status:
-    status += words.text.strip()
-address = ""
-#if the current step is the origin shipping label address
-if status == "Shipping Label Created, USPS Awaiting Item":
-    original_address = current_step.find_next_siblings(class_="tb-location")
-# if not, get destinations from the first step to the current
-else:
-    # 1st step
-    step = usps_html.find_all(class_="tb-step collapsed")[-1]
-    while (step):
-        address = " "
-        checkpoint_address = step.find(class_="tb-location")
-        for words in checkpoint_address:
+try:
+    # Because the webpage is dynamic, check shipping status of current step html element 
+    current_step = usps_html.find_all(class_="tb-step current-step")[0]
+    # get the shipping status from the html current step
+    shipping_status = current_step.find_all(class_="tb-status-detail")
+    # parcel the shipping status into a single string
+    status = " "
+    for words in shipping_status:
+        status += words.text.strip()
+    #if the current step is the origin shipping label address
+    if status == "Shipping Label Created, USPS Awaiting Item":
+        shipping_address = current_step.find_next_siblings(class_="tb-location")
+        for words in shipping_address:
             address += words.text.strip()
-        if address not in checkpoints:
-            checkpoints.append(address)
-        step = step.find_previous_sibling(class_="tb-step collapsed")
-        print(address + "\n")
         
-
-
-
-print(checkpoints)
+    # if not, get destinations from the first step to the current
+    else:
+        # 1st step
+        step = usps_html.find_all(class_="tb-step collapsed")[-1]
+        while (step):
+            # reset address string
+            address = " "
+            checkpoint_address = step.find(class_="tb-location")
+            for words in checkpoint_address:
+                address += words.text.strip()
+            if address not in checkpoints:
+                checkpoints.append(address)
+            step = step.find_previous_sibling(class_="tb-step collapsed")
+            
+    for checkpoint in checkpoints:
+        print(checkpoint)
+except Exception as e:
+    print(f"An error occurred: {e}")
 
 # Close the browser
 driver.quit()
